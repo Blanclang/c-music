df789f878bbac9d516d84948f20b650ade7a32ba
bailang <bail@zhengtongauto.com>
2019-09-17 16:54:59 +0800

    路由配置

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import { GlobalStyle } from './style';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <GlobalStyle>是的发个</GlobalStyle>
-      </div>
-    );
-  }
+import { renderRoutes } from 'react-router-config';//renderRoutes读取路由配置转化为Route标签
+import routes from './routes/index.js';
+import { HashRouter } from 'react-router-dom';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <GlobalStyle></GlobalStyle>
+//         是的发个
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+  return (
+    <HashRouter>
+      <GlobalStyle></GlobalStyle>
+      { renderRoutes(routes) }
+    </HashRouter>
+  )
 }
 
 export default App;
+
+
+
+
